c8cf108e53794f946ea9229290d858f96824ba32
nikolai-churakov <cni.zip@gmail.com>
2022-08-13 16:03:09 +0300

    add hoc

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import FindPage from "./containers/FindPage/FindPage";
 
-let test: string = 'test';
-test = 'test2';
+
 
 function App() {
   return (
     <div className="App">
+
+      <FindPage/>
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -18,13 +18,12 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-
         >
           Learn React
         </a>
-        <span>Api key= 777</span>
-        <FindPage/>
+
       </header>
+
     </div>
   );
 }
---
+++ src/hoc/Layout.tsx
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import "./Layuot.css";
+
+class Layout extends Component {
+    render() {
+        return (
+            <div className={'Layout'}>
+
+            </div>
+        )
+    }
+}
+
+export default Layout
