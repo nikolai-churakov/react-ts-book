9f447de441db0ca18fee2942d64232b68e4401fd
nikolai-churakov <cni.zip@gmail.com>
2022-09-08 22:27:34 +0300

    05

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import {App} from './App';
-import { store } from './store/store'
-import { Provider } from 'react-redux'
+import {store} from './store/store'
+import {Provider} from 'react-redux'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
