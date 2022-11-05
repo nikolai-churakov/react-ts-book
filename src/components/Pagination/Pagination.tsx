7fe2f0db443de03d27dc792765278faa87814dce
nikolai-churakov <cni.zip@gmail.com>
2022-11-05 17:08:52 +0300

    add new component pag

---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import "./Pagination.css"
+import {useSelector} from "react-redux";
+import {BookList as IBookList} from '../../types'
+import {RootReducer} from "../../store/rootReducer";
+
+export const Pagination = () => {
+
+    const pageNumber = []
+
+    const count: IBookList = useSelector((state: RootReducer) => state.booksItems)
+    const PaginationIndex = Math.ceil(count.totalBooks / 10);
+    console.log('total page pagination', PaginationIndex)
+
+    for (let i= 1; i <= PaginationIndex; i++) {
+        pageNumber.push(i)
+    }
+
+
+
+    if (count.totalBooks === 0) {
+        return (
+            <div/>
+        );
+    }
+
+    return (
+        <div className={'BookList'}>
+            <ul className={'pagination'}>
+                {
+                    pageNumber.map(number => (
+                        <li className={'pageItem'} key={number}>
+                            <a href={'!!!'} className={'pageLink'}>
+                                {number}
+                            </a>
+                        </li>
+                    ))
+                }
+            </ul>
+
+        </div>
+    )
+}
--- src/pages/FindPage/FindPage.tsx
+++ src/pages/FindPage/FindPage.tsx
@@ -1,27 +1,22 @@
 import React from 'react';
 import "./FindPage.css"
 import {SearchBooks} from "../../components/SearchBooks/SearchBooks";
-import { BookList } from "../../components/BookList/BookList";
-
-// import {Pagination} from "../../components/BookList/BookList";
+import {BookList} from "../../components/BookList/BookList";
+import {Pagination} from "../../components/Pagination/Pagination";
 
 export const FindPage = () => {
 
     return (
         <div>
             <div className={'FindPage'}>
-
-
                 <SearchBooks/>
             </div>
-
-            {/*// <div className={'Pagination'}>*/}
-            {/*//     <BooksList/>*/}
-            {/*// </div>*/}
-
             <div className={'ListResult'}>
                 <BookList/>
             </div>
+            <div className={'Pagination'}>
+                 <Pagination/>
+             </div>
         </div>
     );
 }
