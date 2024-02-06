c7f14e3403a5b44c71fb1f86a7c109da467cc3fb
nikolai-churakov <cni.zip@gmail.com>
2024-02-06 21:28:01 +0300

    reformat

--- src/components/BookList/BookList.tsx
+++ src/components/BookList/BookList.tsx
@@ -6,36 +6,36 @@ import {RootReducer} from "../../store/rootReducer";
 
 export const BookList = () => {
 
-    const count: IBookList = useSelector((state: RootReducer) => state.booksItems)
+  const count: IBookList = useSelector((state: RootReducer) => state.booksItems)
 
-    if (count.totalBooks === 0) {
-        return (
-            <div className={'BookList'}>
-                {/*<h1>*/}
-                {/*    Result list*/}
-                {/*</h1>*/}
-            </div>
-        );
-    }
+  if (count.totalBooks === 0) {
+    return (
+      <div className={'BookList'}>
+        {/*<h1>*/}
+        {/*    Result list*/}
+        {/*</h1>*/}
+      </div>
+    );
+  }
 
-        return (
-            <div className={'BookList'}>
-                <h1> Found {count.totalBooks} results </h1>
-                <div className={'whiteLineHr'}></div>
-                <div className={'AllBooks'}>
-                    {count.books.map((book, index) =>
-                        <div key={index} className={'Book'}>
-                            <div className={'Image'}
-                                 style={{
-                                     backgroundImage: `url(${count.books[index].volumeInfo.imageLinks.thumbnail || count.books[index].volumeInfo.imageLinks.smallThumbnail})`
-                                 }}>
-                            </div>
-                            <div className={'categoriesDiv'}>{count.books[index].volumeInfo.categories}</div>
-                            <span>{count.books[index].volumeInfo.authors}</span>
-                            <span>{count.books[index].volumeInfo.title}</span>
-                        </div>
-                    )}
-                </div>
+  return (
+    <div className={'BookList'}>
+      <h1> Found {count.totalBooks} results </h1>
+      <div className={'whiteLineHr'}></div>
+      <div className={'AllBooks'}>
+        {count.books.map((book, index) =>
+          <div key={index} className={'Book'}>
+            <div className={'Image'}
+                 style={{
+                   backgroundImage: `url(${count.books[index].volumeInfo.imageLinks.thumbnail || count.books[index].volumeInfo.imageLinks.smallThumbnail})`
+                 }}>
             </div>
-        )
-    }
+            <div className={'categoriesDiv'}>{count.books[index].volumeInfo.categories}</div>
+            <span>{count.books[index].volumeInfo.authors}</span>
+            <span>{count.books[index].volumeInfo.title}</span>
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
