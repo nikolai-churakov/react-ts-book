37b45e89ea32fdb04c2ef96691cd0671228c7eeb
nikolai-churakov <cni.zip@gmail.com>
2024-02-06 21:17:57 +0300

    reformat

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,34 +1,32 @@
-import {useState} from "react";
-
 export interface BaseSelect {
-    text: string;
-    value: string;
+  text: string;
+  value: string;
 }
 
 export interface BookList {
-    books: Book[];
-    totalBooks: number;
+  books: Book[];
+  totalBooks: number;
 }
 
 export interface Request {
-    getSearch: string;
-    selectedTheme: string;
-    sortedResult: string;
-    startIndex: 0;
+  getSearch: string;
+  selectedTheme: string;
+  sortedResult: string;
+  startIndex: 0;
 }
 
 export interface Book {
-    volumeInfo: VolumeInfo;
+  volumeInfo: VolumeInfo;
 }
 
 export interface VolumeInfo {
-    title: string;
-    authors: string[];
-    categories: string[];
-    imageLinks: ImageLinks
+  title: string;
+  authors: string[];
+  categories: string[];
+  imageLinks: ImageLinks
 }
 
 export interface ImageLinks {
-    thumbnail: string;
-    smallThumbnail: string;
+  thumbnail: string;
+  smallThumbnail: string;
 }
