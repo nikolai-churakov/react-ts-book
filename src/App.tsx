cf26413f116cf64a083ea4f255fb4c7b713df000
nikolai-churakov <cni.zip@gmail.com>
2024-02-06 21:17:36 +0300

    reformat

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import {FindPage} from "./pages";
 
 export const App = () => {
-    return (
-            <div className="App">
-                <FindPage/>
-            </div>
-    );
+  return (
+    <div className="App">
+      <FindPage/>
+    </div>
+  );
 }
