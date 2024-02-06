9073ef7d344f6004b5d4a231c5f880788e97c54d
nikolai-churakov <cni.zip@gmail.com>
2024-02-06 21:16:28 +0300

    reformat

--- src/components/UI/Input/Input.tsx
+++ src/components/UI/Input/Input.tsx
@@ -2,19 +2,19 @@ import React, {FC} from "react";
 import "./Input.css"
 
 interface InputProps {
-    value: string;
-    type?: string;
-    onChange: React.ChangeEventHandler<HTMLInputElement>;
+  value: string;
+  type?: string;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
 }
 
-export const Input: FC<InputProps> = ({value, onChange}) =>  {
-    const htmlFor = `${Math.random()}`
+export const Input: FC<InputProps> = ({value, onChange}) => {
+  const htmlFor = `${Math.random()}`
 
-    return (
-            <input className={'InputFind'}
-                id={htmlFor}
-                value={value}
-                onChange={onChange}
-            />
-    )
+  return (
+    <input className={'InputFind'}
+           id={htmlFor}
+           value={value}
+           onChange={onChange}
+    />
+  )
 }
