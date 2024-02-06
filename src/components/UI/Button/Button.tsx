d0a43692e75409daaf0aae925ea931f5c3e67fea
nikolai-churakov <cni.zip@gmail.com>
2024-02-06 21:15:55 +0300

    reformat

--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -2,16 +2,16 @@ import React, {FC} from "react";
 import './Button.css'
 
 interface ButtonProps {
-    onClick: React.MouseEventHandler<HTMLDivElement>;
+  onClick: React.MouseEventHandler<HTMLDivElement>;
 }
 
 export const Button: FC<ButtonProps> = ({onClick}) => {
-    const htmlFor = `${Math.random()}`
-    return (
-        <div className={'Button'} onClick={onClick}>
-            <div
-                id={htmlFor}
-            ></div>
-        </div>
-    )
+  const htmlFor = `${Math.random()}`
+  return (
+    <div className={'Button'} onClick={onClick}>
+      <div
+        id={htmlFor}
+      ></div>
+    </div>
+  )
 }
