c8198a1ad89d48b838e72d5f0889670e6cc59646
nikolai-churakov <cni.zip@gmail.com>
2024-02-06 21:17:05 +0300

    reformat

--- src/components/UI/Select/Select.tsx
+++ src/components/UI/Select/Select.tsx
@@ -3,35 +3,35 @@ import './Select.css'
 import {BaseSelect} from "../../../types";
 
 interface SelectProps {
-    label: string;
-    value: string;
-    onChange: React.ChangeEventHandler<HTMLSelectElement>;
-    options: BaseSelect[];
+  label: string;
+  value: string;
+  onChange: React.ChangeEventHandler<HTMLSelectElement>;
+  options: BaseSelect[];
 }
 
 export const Select: FC<SelectProps> = ({label, value, onChange, options}) => {
-    const htmlFor = `${label} - ${Math.random()}`
+  const htmlFor = `${label} - ${Math.random()}`
 
-    return (
-        <div className={'Select'}>
-            <label htmlFor={htmlFor}>{label}</label>
-            <select
-                id={htmlFor}
-                value={value}
-                onChange={onChange}
+  return (
+    <div className={'Select'}>
+      <label htmlFor={htmlFor}>{label}</label>
+      <select
+        id={htmlFor}
+        value={value}
+        onChange={onChange}
+      >
+        {options.map((option: any, index: number) => {
+          return (
+            <option
+              value={option.value}
+              key={option.value + index}
             >
-                {options.map((option: any, index: number) => {
-                    return (
-                        <option
-                            value={option.value}
-                            key={option.value + index}
-                        >
-                            {option.text}
-                        </option>
-                    )
-                })
-                }
-            </select>
-        </div>
-    )
+              {option.text}
+            </option>
+          )
+        })
+        }
+      </select>
+    </div>
+  )
 }
